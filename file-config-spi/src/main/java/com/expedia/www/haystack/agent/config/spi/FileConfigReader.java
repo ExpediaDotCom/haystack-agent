97cf2ff6871f860a3e2f6066df0667eb1b4ae089
Ashish Aggarwal <asaggarwal@DELC02T84M5G8WN.sea.corp.expecn.com>
2017-10-31 12:48:27 +0200

    Refactoring the agent core libraries

---
+++ file-config-spi/src/main/java/com/expedia/www/haystack/agent/config/spi/FileConfigReader.java
@@ -0,0 +1,64 @@
+/*
+ *  Copyright 2017 Expedia, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+
+package com.expedia.www.haystack.agent.config.spi;
+
+import com.expedia.www.haystack.agent.core.config.Config;
+import com.expedia.www.haystack.agent.core.config.ConfigReader;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
+/**
+ * loads the config for all agents from file. It uses system property, environment variable and default config file
+ * in the given order to find out config file path.
+ */
+public class FileConfigReader implements ConfigReader {
+    private final static Logger LOGGER = LoggerFactory.getLogger(FileConfigReader.class);
+
+    private final static String DEFAULT_CONFIG_FILE_PATH = "agent-config.yaml";
+    private final static String HAYSTACK_AGENT_CONFIG_FILE_PATH = "HAYSTACK_AGENT_CONFIG_FILE_PATH";
+
+    @Override
+    public String getName() {
+        return "file";
+    }
+
+    @Override
+    public Config read() throws Exception {
+        final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+
+        // fallback to the default config path
+        String configFilePath = System.getProperty(HAYSTACK_AGENT_CONFIG_FILE_PATH);
+        if (StringUtils.isEmpty(configFilePath)) {
+            configFilePath = System.getenv(HAYSTACK_AGENT_CONFIG_FILE_PATH);
+            if (StringUtils.isEmpty(configFilePath)) {
+                LOGGER.info("Neither system property nor environment variable is found for haystack agent config file, falling to default file path={}", configFilePath);
+                configFilePath = DEFAULT_CONFIG_FILE_PATH;
+            } else {
+                LOGGER.info("Environment variable for haystack agent config file path found with value={}", configFilePath);
+            }
+        } else {
+            LOGGER.info("System property for haystack agent config file path found with value={}", configFilePath);
+        }
+        return mapper.readValue(new File(configFilePath), Config.class);
+    }
+}
---
+++ span-agent-spi/src/main/java/com/expedia/www/haystack/agent/span/service/SpanAgentGrpcService.java
@@ -0,0 +1,66 @@
+/*
+ *  Copyright 2017 Expedia, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+
+package com.expedia.www.haystack.agent.span.service;
+
+import com.expedia.open.tracing.Span;
+import com.expedia.open.tracing.agent.api.DispatchResult;
+import com.expedia.open.tracing.agent.api.SpanAgentGrpc;
+import com.expedia.www.haystack.agent.core.dispatcher.Dispatcher;
+import io.grpc.stub.StreamObserver;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Validate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class SpanAgentGrpcService extends SpanAgentGrpc.SpanAgentImplBase {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(SpanAgentGrpcService.class);
+
+    private final List<Dispatcher> dispatchers;
+
+    public SpanAgentGrpcService(final List<Dispatcher> dispatchers) {
+        Validate.notEmpty(dispatchers);
+        this.dispatchers = dispatchers;
+    }
+
+    @Override
+    public void dispatch(Span record, StreamObserver<DispatchResult> responseObserver) {
+        final DispatchResult.Builder result = DispatchResult.newBuilder().setCode(DispatchResult.ResultCode.SUCCESS);
+        final StringBuilder failedDispatchers = new StringBuilder();
+
+        for(Dispatcher d : dispatchers) {
+            try {
+                d.dispatch(record);
+            } catch (Exception ex) {
+                LOGGER.error("Fail to dispatch the span record to the dispatcher with name={}", d.getName(), ex);
+                failedDispatchers.append(d.getName()).append(",");
+            }
+        }
+
+        if(failedDispatchers.length() > 0) {
+            result.setCode(DispatchResult.ResultCode.ERROR);
+            result.setErrorMessage("Fail to dispatch the span record to the dispatchers=" +
+                    StringUtils.removeEnd(failedDispatchers.toString(), ","));
+        }
+
+        responseObserver.onNext(result.build());
+        responseObserver.onCompleted();
+    }
+}
