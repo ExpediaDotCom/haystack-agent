b5ff77466c6e52fa0f974d870079e59d9febcc09
Ashish <ashish@tilth.co>
2019-08-28 09:54:54 +0530

    fix unit test

--- agent-providers/span/src/test/scala/com/expedia/www/haystack/agent/pitchfork/HttpConfigSpec.scala
+++ agent-providers/span/src/test/scala/com/expedia/www/haystack/agent/pitchfork/HttpConfigSpec.scala
@@ -12,14 +12,14 @@ import scala.collection.JavaConverters._
 class HttpConfigSpec extends FunSpec with Matchers with EasyMockSugar {
   describe("Http configuration provider") {
     it("should return gzip enabled as true if provided and its value is 'true'") {
-      val config = ConfigFactory.parseMap(Map("port" -> 9115, "http.threads.min" -> 2, "http.threads.max" -> 4, "gzip.enabled" -> true).asJava)
+      val config = ConfigFactory.parseMap(Map("port" -> 9115, "http.threads.min" -> 2, "http.threads.max" -> 4, "gzip.enabled" -> false).asJava)
       val httpConfig = HttpConfig.from(config)
-      httpConfig.isGzipEnabled should equal (true)
+      httpConfig.isGzipEnabled should equal (false)
     }
     it("should return gzip enabled as false if not provided")  {
       val config = ConfigFactory.parseMap(Map("port" -> 9115, "http.threads.min" -> 2, "http.threads.max" -> 4).asJava)
       val httpConfig = HttpConfig.from(config)
-      httpConfig.isGzipEnabled should equal (false)
+      httpConfig.isGzipEnabled should equal (true)
     }
     it("should return gzip buffer as 16Kb if not provided")  {
       val config = ConfigFactory.parseMap(Map("port" -> 9115, "http.threads.min" -> 2, "http.threads.max" -> 4).asJava)
